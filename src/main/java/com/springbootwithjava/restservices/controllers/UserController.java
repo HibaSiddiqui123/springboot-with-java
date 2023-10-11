2e9434bafbdc166aaaa500498c635d68372fe5c6
Hiba <hiba.siddiqui@10pearls.com>
2023-10-11 13:08:06 +0500

    Implementing Exception Handling with Response Status Exception

--- src/main/java/com/springbootwithjava/restservices/controllers/UserController.java
+++ src/main/java/com/springbootwithjava/restservices/controllers/UserController.java
@@ -3,10 +3,16 @@ package com.springbootwithjava.restservices.controllers;
 
 //Controller
 
+import com.springbootwithjava.restservices.ExceptionHandling;
 import com.springbootwithjava.restservices.entities.User;
 import com.springbootwithjava.restservices.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,23 +32,47 @@ public class UserController {
     //create user method
     //@RequestBody Annotation
     //@PostMapping Annotation
+
     @PostMapping("/users")
-    public  User createUser(@RequestBody User user){
-        return userService.createUser(user);
+
+    public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder builder){
+        try{
+             userService.createUser(user);
+            HttpHeaders headers = new HttpHeaders();
+            //setting location header
+            headers.setLocation(builder.path("/users/{id}").buildAndExpand(user.getId()).toUri());
+            //returning response entity
+            return new ResponseEntity(headers, HttpStatus.CREATED);
+
+
+        }catch(ExceptionHandling eh){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, eh.getMessage());
+        }
+
     }
+
+
     //getUserById
     //path variable id will help to get user by id
     //way 1: we can write method and then annotate it with get mapping
     //way 2: otherwise we can use same value of id in annotation and in method name
     @GetMapping("/users/{id}")
     public Optional<User> getUserById(@PathVariable("id")Long id){
-        return  userService.getUserById(id);
+        try {
+            return  userService.getUserById(id);
+
+        }catch(ExceptionHandling eh){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, eh.getMessage());
+        }
     }
     //updateUserById
     @PutMapping("/users/{id}")
     public  User UpdateUserById(@PathVariable("id")Long id, @RequestBody User user){
-        //we will update user and then send it to the rest client
-        return userService.updateUserById(id, user);
+        try{
+            return  userService.updateUserById(id, user);
+        }catch(ExceptionHandling eh){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, eh.getMessage());
+        }
     }
 
     //delete users
